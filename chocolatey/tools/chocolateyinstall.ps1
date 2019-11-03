ad9aaa6b464004f4975684e87b8fe6fcd3fb60f9
Pavel Stefanov <pasha.i.stefanov@gmail.com>
2019-11-03 22:39:05 +0300

    Remove install and uninstall scripts

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,8 +1,38 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿#Requires -RunAsAdministrator
+
+$ErrorActionPreference = 'Stop';
+
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChocolateyUnzip "$toolsDir\src.zip" "$toolsDir\src"
 
-Start-ChocolateyProcessAsAdmin "& `'$toolsDir\scripts\install.ps1`'" -WorkingDirectory $toolsDir
+$NoteBarDir = "$env:Programfiles\NoteBar"
+
+# Copy src
+if (!(Test-Path -Path $NoteBarDir )) {
+    New-Item -ItemType Directory -Path $NoteBarDir    
+}
+Get-ChildItem -Path "src" | Copy-Item -Destination $NoteBarDir
+
+# Add NoteBar to PATH
+[Environment]::SetEnvironmentVariable("Path", "$env:Path;$NoteBarDir", [System.EnvironmentVariableTarget]::Machine)
+
+# Add EventSource for NoteBar logging
+if (!([System.Diagnostics.EventLog]::SourceExists("NoteBar"))) {
+    [System.Diagnostics.EventLog]::CreateEventSource("NoteBar", "Application")    
+}
+
+# Add NoteBar folder for local icons
+$NoteBarAppDataDir = "$env:APPDATA\NoteBar"
+if (!(Test-Path -Path $NoteBarAppDataDir )) {
+    New-Item -ItemType Directory -Path $NoteBarAppDataDir
+}
+
+# Get regasm path
+$dotnetPath = [System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+$RegasmPath = "$dotnetPath\RegAsm.exe"
+
+# Registre NoteBar deskband
+& $RegasmPath /codebase "$NoteBarDir\NoteBar.Toolbar.dll"
 
 Remove-Item -Path "$toolsDir\src" -Recurse -Force
--- chocolatey/tools/chocolateyuninstall.ps1
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -1,4 +1,28 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿#Requires -RunAsAdministrator
 
-Start-ChocolateyProcessAsAdmin "& `'$toolsDir\scripts\uninstall.ps1`'"
+$ErrorActionPreference = 'Stop';
+
+$NoteBarDir = "$env:Programfiles\NoteBar"
+
+# Get regasm path
+$dotnetPath = [System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+$RegasmPath = "$dotnetPath\RegAsm.exe"
+
+# Unregistre notebar deskband
+& $RegasmPath /u "$NoteBarDir\NoteBar.Toolbar.dll"
+Stop-Process -ProcessName explorer
+Start-Sleep -s 2
+
+# Delete NoteBar AppData folder
+Remove-Item -Path "$env:APPDATA\NoteBar" -Recurse -Force
+
+# Delete EventSource of NoteBar logging
+[System.Diagnostics.EventLog]::DeleteEventSource("NoteBar")
+
+# Delete notebar from PATH
+$Path = [Environment]::GetEnvironmentVariable("Path")
+$NewPath = ($Path.Split(";") | Where-Object { $_ -ne $NoteBarDir }) -join ";"
+[Environment]::SetEnvironmentVariable("Path", $NewPath, [System.EnvironmentVariableTarget]::Machine)
+
+# Delete src
+Remove-Item -Path $NoteBarDir -Recurse -Force
